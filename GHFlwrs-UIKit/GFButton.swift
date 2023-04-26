abca40ba24bdf4e7da98e742bd2cbd53f2ef3257
Denys Triasunov <dendden@users.noreply.github.com>
2023-04-26 16:05:49 +0300

    Created styled 'GFButton' and 'GFTextField'

---
+++ GHFlwrs-UIKit/GFButton.swift
@@ -0,0 +1,45 @@
+//
+//  GFButton.swift
+//  GHFlwrs-UIKit
+//
+//  Created by Денис Трясунов on 26.04.2023.
+//
+
+import UIKit
+
+/// A styled Github Followers button.
+///
+/// Default modifiers include:
+/// + *white* text color
+/// + *.headline* text font
+/// + corner radius of 10
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    // Storyboard initializer:
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(buttonColor: UIColor, title: String) {
+        super.init(frame: .zero)
+
+        self.backgroundColor = buttonColor
+        self.setTitle(title, for: .normal)
+
+        configure()
+    }
+
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        // constraints will be configured programmatically with AutoLayout:
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ GHFlwrs-UIKit/GFTextField.swift
@@ -0,0 +1,54 @@
+//
+//  GFTextField.swift
+//  GHFlwrs-UIKit
+//
+//  Created by Денис Трясунов on 26.04.2023.
+//
+
+import UIKit
+
+/// A style Github Followers Text Field.
+///
+/// Default modifiers include:
+/// + **Font**: '.title2', lineLimit: 1, min. font size: 12, text alignment: '.center'
+/// + autocorrection disabled
+/// + cursor tint and text color set to `.label`
+/// + background color of `tertiarySystemBackground`
+/// + border color of `systemGray4`, border width of 2
+/// + corner radius of 10
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+
+        let searchImage = UIImage(systemName: "magnifyingglass")!
+        let placeholderString = NSMutableAttributedString()
+        placeholderString.append(NSAttributedString(attachment: NSTextAttachment(image: searchImage)))
+        placeholderString.append(NSAttributedString(string: " Search username"))
+
+        attributedPlaceholder = placeholderString
+    }
+}
